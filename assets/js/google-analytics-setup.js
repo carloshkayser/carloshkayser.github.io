987b28af8d2ba4299dde1bde1758e2492122f247
Carlos Kayser <carloshkayser@gmail.com>
2025-08-31 18:59:10 -0300

    Add function to capture 'utm_source' parameter and send to GA4

--- assets/js/google-analytics-setup.js
+++ assets/js/google-analytics-setup.js
@@ -4,3 +4,17 @@ function gtag() {
 }
 gtag("js", new Date());
 gtag("config", "{{ site.google_analytics }}");
+
+// Function to get URL query parameter
+function getQueryParam(param) {
+  const urlParams = new URLSearchParams(window.location.search);
+  return urlParams.get(param);
+}
+
+// Capture the 'source' parameter and send to GA4
+const source = getQueryParam('utm_source');
+if (source) {
+  gtag('event', 'page_view', {
+    'utm_source': source
+  });
+}
